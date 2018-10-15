d71ba920e44c20d8df64859d39e1192e3ab1e106
Cosimo Lupo <cosimo@anthrotype.com>
2018-10-15 18:57:48 +0100

    make subprocess.run-like API work on python2.7 as well

--- src/python/ots/__main__.py
+++ src/python/ots/__main__.py
@@ -1,29 +1,11 @@
 import sys
 import ots
-import argparse
 
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    parser = argparse.ArgumentParser(
-        prog="ots",
-        description=(
-            "command-line interface for the OpenType Sanitizer\n\n"
-            "positional arguments:\n"
-            "  font_file       the input font file\n"
-            "  dest_font_file  optional destination file\n"
-            "  font_index      select font in OpenType Collections\n"
-        ),
-        usage="ots [-h] [--version] font_file [dest_font_file] [font_index]",
-        formatter_class=argparse.RawTextHelpFormatter,
-    )
-    parser.add_argument("--version", action="version", version=ots.__version__)
-    _, args = parser.parse_known_args(args)
-    if args:
-        return ots.sanitize(*args)
-    else:
-        parser.print_usage()
+    return ots.sanitize(*args).returncode
 
 
 if __name__ == "__main__":
--- tests/test_ots.py
+++ tests/test_ots.py
@@ -3,4 +3,9 @@ import ots
 
 
 def test_sanitize():
-    assert ots.sanitize() == 1
+    p = ots.sanitize()
+    assert p.returncode == 1
+    assert len(p.args) == 1
+    assert p.args[0].endswith("ots-sanitize")
+    assert p.stdout == None
+    assert p.stderr == None
