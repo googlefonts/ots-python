40edce22e4695be9b7b11d65a62c15541c781ad7
Cosimo Lupo <cosimo@anthrotype.com>
2018-10-15 19:03:36 +0100

    add more tests

--- tests/test_ots.py
+++ tests/test_ots.py
@@ -1,4 +1,6 @@
+from __future__ import absolute_import, unicode_literals
 import pytest
+import subprocess
 import ots
 
 
@@ -9,3 +11,15 @@ def test_sanitize():
     assert p.args[0].endswith("ots-sanitize")
     assert p.stdout == None
     assert p.stderr == None
+
+
+def test_check_error():
+    with pytest.raises(subprocess.CalledProcessError):
+        ots.sanitize("--foo", "bar", check=True)
+
+
+def test_capture_output():
+    p = ots.sanitize(capture_output=True)
+    assert len(p.stdout) == 0
+    stderr = p.stderr.decode()
+    assert stderr.startswith("Usage: ")
