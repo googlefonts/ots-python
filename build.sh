32fe05e5be975b6a9c5e4cf23ba3c06387d18c08
Cosimo Lupo <cosimo@anthrotype.com>
2018-10-15 13:12:08 +0100

    removed build.sh as no longer needed

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BUILD_ROOT=build
-BUILD_DIR=$BUILD_ROOT/meson
-SRC_DIR=src/c/ots
-
-meson --unity=on --buildtype=release --strip -Ddebug=true $BUILD_DIR $SRC_DIR
-ninja -C $BUILD_DIR $1
