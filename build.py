31e4999b843166f65f1cd0d49af5e637b6d009b7
Cosimo Lupo <clupo@google.com>
2021-08-13 16:07:46 +0200

    support cross-compiling for arm64 and build universal2 mac wheel

--- build.py
+++ build.py
@@ -5,6 +5,8 @@ NOTE: This script requires Python 3.6 or above. However the generated binary
 is independent from the python version used to run it.
 """
 import sys
+import platform
+import enum
 from pathlib import Path
 import os
 import subprocess
@@ -13,10 +15,23 @@ import errno
 import argparse
 
 
+class MacTarget(enum.Enum):
+    X86_64 = "x86_64"
+    ARM64 = "arm64"
+    UNIVERSAL2 = "universal2"
+
+
+PLATFORM = platform.uname()
+
 ROOT = Path(__file__).parent.resolve()
-BUILD_ROOT = ROOT / "build"
-BUILD_DIR = BUILD_ROOT / "meson"
 SRC_DIR = ROOT.joinpath("src", "c", "ots")
+DEFAULT_BUILD_DIR = ROOT / "build" / "meson"
+
+CROSS_FILES_DIR = ROOT / "cross-files"
+MACOS_CROSS_FILES = {
+    MacTarget.ARM64: CROSS_FILES_DIR / "darwin" / "arm64",
+    MacTarget.X86_64: CROSS_FILES_DIR / "darwin" / "x86_64",
+}
 
 TOOLS = {
     "meson": os.environ.get("MESON_EXE", "meson"),
@@ -29,12 +44,21 @@ MESON_OPTIONS = [
     "--strip",
     "-Ddebug=true",
 ]
-if sys.platform == "win32":
+if PLATFORM.system == "win32":
     MESON_OPTIONS.append("--force-fallback-for=zlib")
 
-MESON_CMD = [TOOLS["meson"]] + MESON_OPTIONS + [str(BUILD_DIR), str(SRC_DIR)]
-
-NINJA_CMD = [TOOLS["ninja"], "-C", str(BUILD_DIR)]
+if PLATFORM.system == "Darwin":
+    native_machine = MacTarget(PLATFORM.machine)
+    cross_machine = (
+        MacTarget.ARM64 if native_machine == MacTarget.X86_64 else MacTarget.X86_64
+    )
+    MACOS_CROSS_FILES = {
+        native_machine: [""],
+        cross_machine: [MACOS_CROSS_FILES[cross_machine]],
+    }
+    MACOS_CROSS_FILES[MacTarget.UNIVERSAL2] = (
+        MACOS_CROSS_FILES[MacTarget.X86_64] + MACOS_CROSS_FILES[MacTarget.ARM64]
+    )
 
 
 class ExecutableNotFound(FileNotFoundError):
@@ -49,36 +73,86 @@ def check_tools():
             raise ExecutableNotFound(name, path)
 
 
-def configure(reconfigure=False):
-    if not (BUILD_DIR / "build.ninja").exists():
-        subprocess.run(MESON_CMD, check=True, env=os.environ)
+def configure(build_dir, reconfigure=False, cross_file=""):
+    meson_cmd = [TOOLS["meson"]] + MESON_OPTIONS + [str(build_dir), str(SRC_DIR)]
+    if cross_file:
+        meson_cmd.insert(1, f"--cross-file={cross_file}")
+    if not (build_dir / "build.ninja").exists():
+        subprocess.run(meson_cmd, check=True, env=os.environ)
     elif reconfigure:
-        subprocess.run(MESON_CMD + ["--reconfigure"], check=True, env=os.environ)
+        subprocess.run(meson_cmd + ["--reconfigure"], check=True, env=os.environ)
 
 
-def make(*targets, clean=False):
+def make(build_dir, *targets, clean=False):
+    ninja_cmd = [TOOLS["ninja"], "-C", str(build_dir)]
     targets = list(targets)
     if clean:
         subprocess.run(
-            NINJA_CMD + ["-t", "clean"] + targets, check=True, env=os.environ
+            ninja_cmd + ["-t", "clean"] + targets, check=True, env=os.environ
         )
-    subprocess.run(NINJA_CMD + targets, check=True, env=os.environ)
+    subprocess.run(ninja_cmd + targets, check=True, env=os.environ)
 
 
 def main(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--force", action="store_true")
+    parser.add_argument("--build-dir", default=DEFAULT_BUILD_DIR, type=Path)
+    parser.add_argument(
+        "--mac-target",
+        default=None,
+        metavar="ARCH",
+        type=MacTarget,
+        help=(f"{(', ').join(v.value for v in MacTarget)}"),
+    )
     parser.add_argument("targets", nargs="*")
     options = parser.parse_args(args)
 
     check_tools()
 
-    try:
-        configure(reconfigure=options.force)
+    build_dirs = [options.build_dir]
+    cross_files = [""]
 
-        make(*options.targets, clean=options.force)
-    except subprocess.CalledProcessError as e:
-        return e.returncode
+    if options.mac_target is not None:
+        if PLATFORM.system == "Darwin":
+            cross_files = MACOS_CROSS_FILES[options.mac_target]
+            if options.mac_target == MacTarget.UNIVERSAL2:
+                build_dirs = [options.build_dir / arch for arch in ("x86_64", "arm64")]
+        else:
+            print(
+                "WARNING: --mac-target option is ignored on non-mac platforms",
+                file=sys.stderr,
+            )
+
+    for cross_file, build_dir in zip(cross_files, build_dirs):
+        try:
+            configure(
+                build_dir,
+                reconfigure=options.force,
+                cross_file=cross_file,
+            )
+
+            make(build_dir, *options.targets, clean=options.force)
+        except subprocess.CalledProcessError as e:
+            return e.returncode
+
+    if options.mac_target == MacTarget.UNIVERSAL2:
+        # create universal binary by merging multiple archs with the 'lipo' tool:
+        # https://developer.apple.com/documentation/apple-silicon/building-a-universal-macos-binary
+        for filename in options.targets:
+            arch_paths = [
+                options.build_dir / arch / filename for arch in ("x86_64", "arm64")
+            ]
+            dest_path = options.build_dir / filename
+            subprocess.run(
+                ["lipo", "-create", "-output", dest_path] + arch_paths,
+                check=True,
+                env=os.environ,
+            )
+            # confirm that we got a 'fat' binary
+            result = subprocess.run(
+                ["lipo", "-archs", dest_path], check=True, capture_output=True
+            )
+            assert "x86_64 arm64" == result.stdout.decode().strip()
 
 
 if __name__ == "__main__":
